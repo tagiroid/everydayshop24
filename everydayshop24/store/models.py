5e798f807f6c9a23db820cfdb1f0cdedd0d19e32
tagiroid <tagiroid@gmail.com>
2023-04-08 03:47:06 +0300

    name email added to django admin

--- everydayshop24/store/models.py
+++ everydayshop24/store/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Customer(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.email)
