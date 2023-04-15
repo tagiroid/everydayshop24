43f72107816fc2a820b93e656217cbc858ca08a2
tagiroid <tagiroid@gmail.com>
2023-04-15 23:44:11 +0300

    one line of code was added

--- everydayshop24/orders/models.py
+++ everydayshop24/orders/models.py
@@ -5,5 +5,6 @@ class Customer(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=200)
 
+
     def __str__(self):
         return "%s %s" % (self.name, self.email)
