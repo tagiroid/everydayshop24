6367a6596227f6dc042ea40c4e1e26176f83ff49
tagiroid <tagiroid@gmail.com>
2023-04-19 19:45:45 +0300

    product section

--- everydayshop24/orders/models.py
+++ everydayshop24/orders/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
 
-class Customer(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=200)
-
+class Order(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True, default=None)
+    email = models.EmailField(blank=True, null=True, default=None)
+    phone = models.CharField(max_length=48, blank=True, null=True, default=None)
+    comment = models.TextField(blank=True, null=True, default=None)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "%s %s" % (self.name, self.email)
+        return "Заказ %s" % self.id
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class ProductInOrder(models.Model):
+    order = models.ForeignKey(Order, blank=True, null=True, default=None)
+    product = models.ForeignKey(Product, blank=True, null=True, default=Non)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.product.name
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
--- everydayshop24/products/models.py
+++ everydayshop24/products/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 
-class Customer(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=200)
+class Product(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    description = models.TextField(blank=True, null=True, default=None)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "%s %s" % (self.name, self.email)
+        return "%s" % self.id
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, blank=True, null=True, default=None)
+    image = models.ImageField(upload_to='/products_images/')
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
