ed5fbb2d12632ae1a00499c5a309080af93015e1
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-06-06 23:46:46 +0500

    comment added

--- everydayshop24/store/urls.py
+++ everydayshop24/store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 
-admin.autodiscover()
+# admin.autodiscover()
 
 urlpatterns = [
     path('', views.store, name="store"),
