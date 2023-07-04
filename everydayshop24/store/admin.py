69657951634faab6fe9ec04f49ac2fd64ce81852
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-07-04 23:38:40 +0500

    todays commit

--- everydayshop24/store/admin.py
+++ everydayshop24/store/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-# this section shows categories in admin site
-# shall i add category?
-# no
 
 admin.site.register(Customer)
 admin.site.register(Product)
