463954f13b6eac4fc0587946e6baefbc7f665897
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-10-11 23:29:29 +0500

    one insertion

--- everydayshop24/static/js/cart.js
+++ everydayshop24/static/js/cart.js
@@ -7,6 +7,7 @@ for (i = 0; i < updateBtns.length; i++){
         console.log('productId:', productId, 'Action:', action)
 
         console.log('USER:', user)
+
         if (user === 'AnonymousUser') {
             addCookieItem(productId, action)
         }
