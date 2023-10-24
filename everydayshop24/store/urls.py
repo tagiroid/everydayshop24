6576c67badafc9a38fdb285ddaee8bade07c98cc
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-10-24 23:09:18 +0500

    urls urlpatterns

--- everydayshop24/store/urls.py
+++ everydayshop24/store/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
