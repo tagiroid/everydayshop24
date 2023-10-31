b1bfd587edcd02a809c83cbf22c33403da55d8d3
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-10-31 21:42:18 +0500

    settings

--- everydayshop24/everydayshop24/settings.py
+++ everydayshop24/everydayshop24/settings.py
@@ -22,7 +22,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'store.apps.StoreConfig',
     'members.apps.MembersConfig',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
