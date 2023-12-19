95b434938a29d0c8959427fb45d921cd776359f6
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-12-19 23:31:33 +0500

    commitcommit

--- everydayshop24/store/models.py
+++ everydayshop24/store/models.py
@@ -40,6 +40,7 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
     @property
     def shipping(self):
         shipping = False
@@ -48,11 +49,13 @@ class Order(models.Model):
             if i.product.digital == False:
                 shipping = True
         return shipping
+
     @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
         return total
+
     @property
     def get_cart_items(self):
         orderitems = self.orderitem_set.all()
