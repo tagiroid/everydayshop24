2216beba011dbd313579c58f2e35665fbd4aa08f
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-12-20 22:57:33 +0500

    settings

--- everydayshop24/everydayshop24/settings.py
+++ everydayshop24/everydayshop24/settings.py
@@ -69,7 +69,9 @@ WSGI_APPLICATION = 'everydayshop24.wsgi.application'
 
 DATABASES = {
     'default': {
+
         'ENGINE': 'django.db.backends.sqlite3',
+
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
