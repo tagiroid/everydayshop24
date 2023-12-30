934e6602aaee46af309deae049fee4aaef9c95eb
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2023-12-30 23:46:10 +0500

    models

--- everydayshop24/store/models.py
+++ everydayshop24/store/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200)
