eee82e3c6473b6f6db186a15180095040862f5cd
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2024-01-07 23:36:56 +0500

    urls

--- everydayshop24/store/urls.py
+++ everydayshop24/store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
