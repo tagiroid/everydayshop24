8c34f3ea73101b6a37d770594da047d46e661c23
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2024-02-03 23:37:22 +0500

    apps

--- everydayshop24/everydayshop24/settings.py
+++ everydayshop24/everydayshop24/settings.py
@@ -18,7 +18,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
     'store.apps.StoreConfig',
     'members.apps.MembersConfig',
     'django.contrib.admin',
@@ -27,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
 ]
 
 MIDDLEWARE = [
