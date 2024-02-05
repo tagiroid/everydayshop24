1f5e70a685aed20b219f1cc4e85c9f1fbc56caeb
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2024-02-05 23:21:01 +0500

    middleware reverse

--- everydayshop24/everydayshop24/settings.py
+++ everydayshop24/everydayshop24/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -37,7 +36,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 
