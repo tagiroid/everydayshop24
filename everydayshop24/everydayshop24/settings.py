9ae25d576ce62f32f5ad43cdd23e4e747803da29
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2024-02-08 23:07:07 +0500

    databss

--- everydayshop24/everydayshop24/settings.py
+++ everydayshop24/everydayshop24/settings.py
@@ -66,11 +66,9 @@ WSGI_APPLICATION = 'everydayshop24.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-
     }
 }
 
