27b3f6131d82d29e9b7cce26c372644135233caa
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2024-03-01 23:18:38 +0500

    Product.model

--- everydayshop24/store/models.py
+++ everydayshop24/store/models.py
@@ -13,7 +13,6 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
-
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=True)
