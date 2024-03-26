8cf36e85f41f7e6123fb87713211b100adbdddc2
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2024-03-26 23:53:28 +0500

    admins

--- everydayshop24/store/admin.py
+++ everydayshop24/store/admin.py
@@ -3,8 +3,12 @@ from .models import *
 
 
 admin.site.register(Customer)
+
 admin.site.register(Product)
+
 admin.site.register(Order)
+
 admin.site.register(OrderItem)
+
 admin.site.register(ShippingAddress)
 
