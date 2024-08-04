53126622b0e82367fa577aa38c6ca96486bb09b4
tagir gilmutdinov <101896416+tagiroid@users.noreply.github.com>
2024-08-04 22:17:15 +0500

    tomorrow yandex

--- everydayshop24/store/admin.py
+++ everydayshop24/store/admin.py
@@ -9,7 +9,3 @@ admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
 
 # delete this comment
-# Но я тысячи раз обрывал провода
-# i tried to practice and i fail
-# so i tried again and fail again
-# i did something today
